31953da347c0c0ab03969444fddbadd3780f534c
TomasLeTrain <tomylopezd@gmail.com>
2024-03-09 22:37:56 -0500

    create models

--- base/models.py
+++ base/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
 # Create your models here.
+
+class Tag(models.Model):
+    tag_name = models.CharField(default="",max_length=200)
+    def __str__(self):
+        return self.tag_name
+
+class Problem(models.Model):
+    problem_name = models.CharField(default="",max_length=200)
+    link = models.charfield(default="",max_length=250)
+    elo = models.IntegerField(default=0)
+    tags = models.ManyToManyField(Tag)
+    editorial = models.charfield(default="",max_length=250)
+    def __str__(self):
+        return self.problem_name
+
+class User(models.Model):
+    username = models.CharField(max_length=50)
+    date_joined = models.DateTimeField("date joined")
+    problems_solved = models.ManyToManyField(Problem)
+    def __str__(self):
+        return self.username
