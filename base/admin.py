cea341127f544f3485d3eebd973594365fc037b0
TomasLeTrain <tomylopezd@gmail.com>
2024-03-10 20:13:57 -0400

    change User class to standard Django User class

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Problem
+from .models import UserInfo
+from .models import Tag
+
+admin.site.register(Problem)
+admin.site.register(UserInfo)
+admin.site.register(Tag)
--- base/models.py
+++ base/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 # Create your models here.
 
 class Tag(models.Model):
@@ -15,9 +16,9 @@ class Problem(models.Model):
     def __str__(self):
         return self.problem_name
 
-class User(models.Model):
-    username = models.CharField(max_length=50)
+class UserInfo(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     date_joined = models.DateTimeField("date joined")
-    problems_solved = models.ManyToManyField(Problem)
+    problems_solved = models.ManyToManyField(Problem,blank=True)
     def __str__(self):
-        return self.username
+        return self.user.username
