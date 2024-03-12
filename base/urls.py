11f140f1caba981d9ecb74d82e6da1c0aa33f08d
TomasLeTrain <tomylopezd@gmail.com>
2024-03-12 16:40:44 -0400

    separated navbar and base html into templates

--- base/urls.py
+++ base/urls.py
@@ -3,8 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home),
-    path("home", views.home, name = "home"),
+    path("", views.home, name = "home"),
     path("problems", views.problems, name = "problems"),
     path("learn", views.learn, name = "learn"),
     path("contests", views.contests, name = "contests"),
