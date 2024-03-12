0a699844c9ed5fc5456960e6a9d3f5f15fe78fb3
TomasLeTrain <tomylopezd@gmail.com>
2024-03-11 21:51:17 -0400

    added base templates

--- base/urls.py
+++ base/urls.py
@@ -3,5 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name = "index")
+    path("", views.home),
+    path("home", views.home, name = "home"),
+    path("problems", views.problems, name = "problems"),
+    path("learn", views.learn, name = "learn"),
+    path("contests", views.contests, name = "contests"),
+    path("login", views.login, name = "login"),
+    path("signup", views.signup, name = "signup"),
 ]
--- base/views.py
+++ base/views.py
@@ -3,9 +3,24 @@ from django.shortcuts import render
 # Create your views here.
 from .models import Problem
 
-def index(request):
+def home(request):
+    return render(request, "base/home.html")
+
+def problems(request):
     problems = Problem.objects.all()
     context = {
         "problems": problems,
     }
-    return render(request, "base/index.html", context)
+    return render(request, "base/problems.html", context)
+
+def contests(request):
+    return render(request, "base/contests.html")
+
+def learn(request):
+    return render(request, "base/learn.html")
+
+def signup(request):
+    return render(request, "base/signup.html")
+
+def login(request):
+    return render(request, "base/login.html")
