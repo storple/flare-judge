3cacad87fb444417bf40ccd49a788ce4f325a743
TomasLeTrain <tomylopezd@gmail.com>
2024-07-02 17:48:31 -0400

    added language support to pages

--- learn/markdown.py
+++ learn/markdown.py
@@ -1,5 +1,7 @@
 import markdown
 
+main_languages = ['cpp','python','java']
+
 def _code_custom_formatter(source, language, class_name, options, md, **kwargs):
     code_block = md.preprocessors['fenced_code_block'].extension.superfences[0]['formatter'](
         source, language, options, md, **kwargs
@@ -9,8 +11,12 @@ def _code_custom_formatter(source, language, class_name, options, md, **kwargs):
 <button class="code-clipboard hover:text-accent-400 active:text-accent-700 transition-colors absolute top-2.5 right-0 md:right-3.5">\
 <i class="fa-regular fa-copy"></i>\
 </button>"""
-    return f'<div class="relative {language}">{clipboard_button}{code_block}</div>'
-    # return code_block
+    if language in main_languages:
+        left_div = f"""<div :class="{{ 'hidden' : $store.language.language != '{language}'}}" class="relative hidden {language}">"""
+    else:
+        left_div = f"""<div class="relative {language}">"""
+
+    return f"""{left_div}{clipboard_button}{code_block}</div>"""
 
 md = markdown.Markdown(
         output_format="html",
@@ -29,12 +35,7 @@ md = markdown.Markdown(
             },
             'pymdownx.extra': {
                 "pymdownx.superfences": {
-                    "custom_fences": [
-                        { "name": "cpp",    "class": "", "format": _code_custom_formatter, },
-                        { "name": "c++",    "class": "", "format": _code_custom_formatter, },
-                        { "name": "python", "class": "", "format": _code_custom_formatter, },
-                        { "name": "java",   "class": "", "format": _code_custom_formatter, },
-                    ]
+                    "custom_fences": [ { "name" : language, "class": "","format": _code_custom_formatter} for language in main_languages]
                 }
             }
         }
