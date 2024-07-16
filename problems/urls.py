d0ff7623e3b91cd46cb12ce5d574f363e484f6ef
TomasLeTrain <tomylopezd@gmail.com>
2024-07-15 22:40:09 -0400

    add support for sorting, add back page support for problems table

--- problems/urls.py
+++ problems/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path("", views.problems_view, name = "problems"),
     # path("page/<int:page>/", views.problems, name = "problems_page"),
     # path("page/<int:page>/", views.filter_problems, name = "problems_page"),
-    path("page/<int:problem_page>/", views.problems_view, name = "problems_page"),
+    # path("page/<int:problem_page>/", views.problems_view, name = "problems_page"),
     path("save_problem/", views.save_problem, name = "save_problem"),
 ]
--- problems/views.py
+++ problems/views.py
@@ -1,97 +1,139 @@
 from django.http import HttpResponse, HttpRequest, QueryDict
+from django.core.paginator import Paginator
 
 from django.shortcuts import render
 from flare.shortcuts import htmx_render, full_page_render
 
 from .models import Problem
 
-def problems_by_page(problems: Problem, page, problem_per_page=50):
-    if page >= 1:
-        return problems[(page-1) * problem_per_page : page * problem_per_page]
-    else:
-        return None
+def filter_problems(request: HttpRequest,data: QueryDict):
+    min_elo = data.get("min_elo","")
+    max_elo = data.get("max_elo","")
+    tags = data.get("tags","")
+    not_completed = data.get("not_completed","") != ""
+    completed = data.get("completed","") != ""
+    name_sort = data.get("name_sort","0")
+    elo_sort = data.get("elo_sort","0")
+    page = data.get("page","1")
 
+    try:
+        page = int(page)
+    except:
+        page = 1
 
-def max_pages(problems: Problem, problems_per_page=50):
-        return max(1, ((problems.count() - 1) // problems_per_page) + 1)
+    name_sort = name_sort if name_sort in ["0","1","2"] else "0"
+    elo_sort = elo_sort if elo_sort in ["0","1","2"] else "0"
 
+    if name_sort != "0" and elo_sort != "0":
+        # only order by one
+        name_sort = "0"
 
-def filter_problems(request: HttpRequest,data: QueryDict, page):
-        min_elo = data.get("min_elo","")
-        max_elo = data.get("max_elo","")
-        tags = data.get("tags","")
-        not_completed = data.get("not_completed","") != ""
-        completed = data.get("completed","") != ""
+    name_sort_dict = {
+        "0":"",
+        "1":"problem_name",
+        "2":"-problem_name",
+    }
+    elo_sort_dict = {
+        "0":"",
+        "1":"elo",
+        "2":"-elo",
+    }
 
-        user = request.user
-        if user.is_authenticated:
-                currentprofile = user.profile
-                if completed and not_completed:
-                    # no filtering
-                    problems = Problem.objects.all()
-                elif completed:
-                    # only show completed ones
-                    problems = currentprofile.problems_solved.all()
-                elif not_completed:
-                    # only show not completed problems
-                    problems = Problem.objects.exclude(profile=currentprofile)
-                else:
-                    # no problems
-                    problems = Problem.objects.none()
+    name_sort_field = name_sort_dict[name_sort]
+    elo_sort_field  = elo_sort_dict[elo_sort]
 
-        if min_elo != "": problems = problems.filter(elo__gte=min_elo)
-        if max_elo != "": problems = problems.filter(elo__lte=max_elo)
-        if tags != "": problems = problems.filter(tags__tag_name__contains=tags)
+    user = request.user
+    if user.is_authenticated:
+        currentprofile = user.profile
+        if completed and not_completed:
+            # no filtering
+            problems = Problem.objects.all()
+        elif completed:
+            # only show completed ones
+            problems = currentprofile.problems_solved.all()
+        elif not_completed:
+            # only show not completed problems
+            problems = Problem.objects.exclude(profile=currentprofile)
+        else:
+            # no problems
+            problems = Problem.objects.none()
 
-        current_max_pages = max_pages(problems)
+    if min_elo != "": problems = problems.filter(elo__gte=min_elo)
+    if max_elo != "": problems = problems.filter(elo__lte=max_elo)
+    if tags != "": problems = problems.filter(tags__tag_name__contains=tags)
 
-        page = min(current_max_pages, page)
+    if name_sort_field != "":
+        problems = problems.order_by(name_sort_field)
+    if elo_sort_field != "":
+        problems = problems.order_by(elo_sort_field)
 
-        problems = problems_by_page(problems,page)
+    paginator = Paginator(problems,30)
+    max_pages = paginator.num_pages
 
-        context = {
-            "problems": problems,
-            "min_elo": min_elo,
-            "max_elo": max_elo,
-            "tags": tags,
-            "not_completed": not_completed,
-            "completed": completed,
-            "problem_page": page,
-            "max_pages": current_max_pages,
-        }
-        return context
+    page = min(max_pages, page)
+    print(page)
 
-def problems_view(request: HttpRequest, problem_page=1):
+    problems = paginator.get_page(page)
+
+    page_range = paginator.get_elided_page_range(page,on_each_side=1, on_ends=1)
+
+    context = {
+        "problems": problems,
+
+        "min_elo": min_elo,
+        "max_elo": max_elo,
+        "tags": tags,
+
+        "not_completed": not_completed,
+        "completed": completed,
+        
+        "name_sort": name_sort,
+        "elo_sort": elo_sort,
+
+        "problem_page": page,
+        "max_pages": max_pages,
+        "page_range": page_range,
+    }
+    return context
+
+def problems_view(request: HttpRequest):
     if request.method == "GET":
         data = request.GET
 
         data_present = len(data) != 0
 
         if data_present:
-            context = filter_problems(request,data,problem_page)
+            context = filter_problems(request,data)
 
             if request.htmx:
                 # on filter url with htmx: only return the table
-                return render(request, "problems/components/problems_table.html", context)
+                context["oob_swap"] = True
+                return render(request, "problems/components/table_update.html", context)
             else:
                 # on filter url without htmx: return the whole page with the filtering
-                print("doing this thing")
                 return full_page_render(request, "problems/problems.html", context, page = "problems")
         else:
             problems = Problem.objects.all()
 
-            current_max_pages = max_pages(problems)
+            paginator = Paginator(problems,30)
+            max_pages = paginator.num_pages
 
-            page = min(current_max_pages, problem_page)
+            page = 1
+            print(page)
+            problems = paginator.get_page(page)
 
-            problems = problems_by_page(problems,page)
+            page_range = paginator.get_elided_page_range(page,on_each_side=1, on_ends=1)
 
             context = {
                 "problems": problems,
-                "problem_page": page,
                 "not_completed": True,
                 "completed": True,
-                "max_pages": current_max_pages,
+                "name_sort": 0,
+                "elo_sort": 0,
+
+                "problem_page": page,
+                "max_pages": max_pages,
+                "page_range": page_range,
             }
             # render page without filtering
             return htmx_render(request, "problems/problems.html", context, page = "problems")
