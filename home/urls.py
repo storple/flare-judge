3daddd5bf2caeeaf3644b0b0778627753ca08a90
TomasLeTrain <tomylopezd@gmail.com>
2024-08-05 20:01:41 -0400

    style error pages, small fixes

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,7 @@
 from django.urls import path
 
 from . import views
-from learn import views as learn_views
 
 urlpatterns = [
     path("", views.home, name = "home"),
-    # path("", learn_views.main_page, name = "home"),
 ]
